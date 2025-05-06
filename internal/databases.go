f59b4734691ca37522950e585727dfbbb2b63d5d
foizzen <pasakarpenko75@gmail.com>
2025-05-06 14:27:12 +0300

    database conn

--- internal/databases.go
+++ internal/databases.go
@@ -1,1 +1,35 @@
 package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func ConnectDb() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("Can`t load data of user")
+	}
+	dsn := fmt.Sprintf(
+        "user=%s password=%s dbname=%s host=%s port=%s sslmode=%s",
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_PORT"),
+        os.Getenv("DB_SSLMODE"),
+    )
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	return db, nil
+}
+
+func AddPost() {
+
+}
