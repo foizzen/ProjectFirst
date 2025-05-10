394982655872972ba7373859067f53d08ca516f0
ShaKe <buturlakinaleksandr56@gmail.com>
2025-05-10 17:21:42 +0300

    slider -- 2nd Commit

---
+++ static/script.js
@@ -0,0 +1,14 @@
+let slider = document.querySelector('.posters-slider')
+const nextButton = document.querySelector('.next-button')
+const previousButton = document.querySelector('.previous-button')
+
+nextButton.addEventListener('click', (event) => {
+    const firstImage = slider.querySelector('a:first-child')
+    slider.append(firstImage)
+
+})
+
+previousButton.addEventListener('click', (event) => {
+    const lastImage = slider.querySelector('a:last-child')
+    slider.prepend(lastImage)
+})
